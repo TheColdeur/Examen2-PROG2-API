2c360ea777c273581468b723c629fcbfad5b7f5a
Rakotoson Avisoa Morgan <hei.avisoa@gmail.com>
2025-09-02 17:02:51 +0300

    Change venv file

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\HEI\S2\PROG2\API\Examen2\venv
+set VIRTUAL_ENV=C:\HEI\S2\PROG2\API\Examen Finale\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
